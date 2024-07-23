bbc89a5940343ec8a54af44d3a87996af23ff193
Shivank Anchal <shivajay295@gmail.com>
2024-07-23 13:36:28 +0200

    Consolidate footer updates and email obfuscation script
    - Added mailto link to footer.
    - Added contact and email links to the footer.
    - Implemented client-side script for email obfuscation.
    - Reformat unscrambleEmail.js and integrated new client module.
    
    Signed-off-by: Shivank Anchal <shivajay295@gmail.com>

---
+++ static/js/unscrambleEmail.js
@@ -0,0 +1,21 @@
+import { unscramble } from "botex";
+
+export function onRouteDidUpdate({ location, previousLocation }) {
+  if (ExecutionEnvironment.canUseDOM) {
+    setTimeout(() => {
+      document.body.addEventListener("click", function (event) {
+        if (event.target.matches('a[href="/#contact-email"]')) {
+          event.preventDefault();
+          const contactEmailKey = "CHKXRg4cdMZ0XADF";
+          const obfuscatedContactEmail =
+            "BwC1311181fB4C1sBzmC14162k1dByC112z16132t1o1jBh";
+          const unscrambledEmail = unscramble(
+            obfuscatedContactEmail,
+            contactEmailKey
+          );
+          window.location.href = `mailto:${unscrambledEmail}`;
+        }
+      });
+    }, 1000); // Delay to ensure the DOM is fully updated
+  }
+}
