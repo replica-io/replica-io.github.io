16c9dcccad34e2a13a15fe59fb2cb3e33f3b4858
Shivank Anchal <shivajay295@gmail.com>
2024-07-30 23:25:08 +0530

    Reformat unscrambleEmail.js
    Signed-off-by: Shivank Anchal <shivajay295@gmail.com>

--- static/js/unscrambleEmail.js
+++ static/js/unscrambleEmail.js
@@ -1,14 +1,18 @@
 import { unscramble } from "botex";
 
 export function onRouteDidUpdate({ location, previousLocation }) {
-  if (typeof window !== 'undefined') {
+  if (typeof window !== "undefined") {
     setTimeout(() => {
       document.body.addEventListener("click", function (event) {
         if (event.target.matches('a[href="/#contact-email"]')) {
           event.preventDefault();
           const contactEmailKey = "CHKXRg4cdMZ0XADF";
-          const obfuscatedContactEmail = "BwC1311181fB4C1sBzmC14162k1dByC112z16132t1o1jBh";
-          const unscrambledEmail = unscramble(obfuscatedContactEmail, contactEmailKey);
+          const obfuscatedContactEmail =
+            "BwC1311181fB4C1sBzmC14162k1dByC112z16132t1o1jBh";
+          const unscrambledEmail = unscramble(
+            obfuscatedContactEmail,
+            contactEmailKey
+          );
           window.location.href = `mailto:${unscrambledEmail}`;
         }
       });
